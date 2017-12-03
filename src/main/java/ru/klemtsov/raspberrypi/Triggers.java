8e654d9a01ad69daed64582c7e875861a957c48f
oleg <klemtsov@gmail.com>
2017-12-03 17:38:58 +0300

    add led

--- src/main/java/ru/klemtsov/raspberrypi/Triggers.java
+++ src/main/java/ru/klemtsov/raspberrypi/Triggers.java
@@ -13,23 +13,27 @@ public class Triggers {
 
     private GpioController gpioController;
     private GpioPinDigitalInput btn;
+    private GpioPinDigitalOutput led;
 
     @PostConstruct
     public void init(){
-        System.out.printf("Запуск класса Triggers");
+        System.out.println("Запуск класса Triggers");
         gpioController = GpioFactory.getInstance();
         btn = gpioController.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN);
         btn.setShutdownOptions(true);
-        btn.setDebounce(100);
+        btn.setDebounce(50);
+
+        led = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01,"LED1", PinState.LOW);
+        led.setShutdownOptions(true);
         btn.addListener(new GpioPinListenerDigital() {
             @Override
             public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent gpioPinDigitalStateChangeEvent) {
                 System.out.println(String.format("%s Кнопка %s", Calendar.getInstance().getTime(), gpioPinDigitalStateChangeEvent.getState()));
+                if (gpioPinDigitalStateChangeEvent.getState().isHigh()){
+                    led.toggle();
+                }
             }
         });
-
-
-
     }
 
     public GpioController getGpioController() {
