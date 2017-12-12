1f7d08dad0cc9b03b7f1268a9df9d1fd23fcd670
oleg <klemtsov@gmail.com>
2017-12-12 21:37:41 +0300

    исправлена ошибка инициализуции

--- src/main/java/ru/klemtsov/raspberrypi/seven_led/SevenLedController.java
+++ src/main/java/ru/klemtsov/raspberrypi/seven_led/SevenLedController.java
@@ -5,11 +5,6 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-
 public class SevenLedController {
 
     private Thread worker;
@@ -17,40 +12,53 @@ public class SevenLedController {
 
     public SevenLedController(GpioController controller) {
         indicator = new SevenLedIndicator();
-        List<GpioPinDigitalOutput> leds = new ArrayList<>(8);
+        GpioPinDigitalOutput[] leds = {
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_00, "LED_A", PinState.HIGH),
 
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_00, "LED_A", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED_B", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_02, "LED_C", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_03, "LED_D", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_04, "LED_E", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_05, "LED_F", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_06, "LED_G", PinState.HIGH));
-        leds.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_07, "LED_DP", PinState.HIGH));
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED_B", PinState.HIGH),
 
-        List<GpioPinDigitalOutput> parts = new ArrayList<>(3);
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_02, "LED_C", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_03, "LED_D", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_04, "LED_E", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_05, "LED_F", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_06, "LED_G", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_07, "LED_DP", PinState.HIGH)
+        };
 
-        parts.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_08, "LED_P1", PinState.HIGH));
-        parts.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_09, "LED_P2", PinState.HIGH));
-        parts.add(controller.provisionDigitalOutputPin(RaspiPin.GPIO_10, "LED_P3", PinState.HIGH));
+        for (GpioPinDigitalOutput pin :
+                leds) {
+            pin.setShutdownOptions(true);
+        }
 
-        indicator.init((GpioPinDigitalOutput[])leds.toArray(), (GpioPinDigitalOutput[])parts.toArray());
+        GpioPinDigitalOutput[] parts = {
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_08, "LED_P1", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_09, "LED_P2", PinState.HIGH),
+                controller.provisionDigitalOutputPin(RaspiPin.GPIO_10, "LED_P3", PinState.HIGH)
+        };
+
+        for (GpioPinDigitalOutput pin :
+                parts) {
+            pin.setShutdownOptions(true);
+        }
+
+
+        indicator.init(leds, parts);
 
         worker = new Thread(indicator);
     }
 
-    public void setValue(int value){
+    public void setValue(int value) {
         indicator.setValue(value);
     }
 
-    public void start(){
+    public void start() {
         worker.start();
     }
 
-    public void stop(){
+    public void stop() {
         worker.interrupt();
         try {
-            wait(1000L );
+            wait(1000L);
             System.out.printf("Worker завершен!\n");
         } catch (InterruptedException e) {
             e.printStackTrace();
